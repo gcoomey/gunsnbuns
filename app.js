95e45aa7ef2a6c2ef91cb10a56e671312407465c
unknown <gracecoomey@gmail.com>
2021-03-20 10:34:31 +0000

    'March20'

--- app.js
+++ app.js
@@ -4,10 +4,20 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var session = require('express-session');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+app.set('trust proxy', 1) // trust first proxy
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true, maxAge: 60000 }
+}))
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,9 +32,63 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.get('/getDriverData', function (req, res) {
+   
+   
+  // put the data in the database
+  // pulling in mysql
+  var mysql = require('mysql');
+   // set up a connection  
+  var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "test",
+  password: ""
+  });
+  
+  
+   
+// hold the data that we going to send back.
+var output = '';
+
+
+  con.connect(function(err) {
+  if (err) throw err;
+  con.query("SELECT orderby, items FROM orders;", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+   
+    
+    // looping over the records
+    for(var i=0; i< result.length; i++){
+        output = output + result[i].orderby + '---' + result[i].items + '<br>';
+    }
+    
+     // return the output variable
+    res.send(output);   
+  });
+});
+
+
+  
+  
+});
+
+app.get('/testSession', function (req, res) {
+	
+	//set
+	req.session.email = 'test@domain.com';
+	
+	//get
+	var temp = req.session.email;
+
+	res.send("hello" + temp);
+	});
+
 app.get('/getManagerData', function (req, res) {
    
-   
+   req.session.manager = 1
+   req.session.email = ''
   // put the data in the database
   // pulling in mysql
   var mysql = require('mysql');
@@ -70,7 +134,16 @@ app.post('/checkTheLogin', function (req, res) {
    // catching the variables
   var username = req.body.username;  
   var pass = req.body.password;
-   
+  
+  //setting the user name into the session
+  req.session.username = username;
+  req.session.validSession = true;
+  
+  var timeLeft = req.session.cookie.maxAge/ 1000;
+  console.log("Time left" + timeLeft);  
+  
+ 
+  
   // put the data in the database
   // pulling in mysql
   var mysql = require('mysql');
@@ -138,10 +211,27 @@ app.post('/putInDatabase', function (req, res) {
   
 })
 
+app.get('/checkIfTimeLeft', function (req, res) {
+var timeLeft = req.session.cookie.maxAge/ 1000;
+console.log(timeLeft);
 
-
+  if(timeLeft<1){
+	  
+	  res.send('expired');
+	  
+  }else{
+	  res.send('ok');
+  }
+  
+});
 
 app.post('/completeCheckout', function (req, res) {
+  
+  var timeLeft = req.session.cookie.maxAge/ 1000;
+  if(timeLeft<100){
+	  
+  }
+  
   
   // catching the variables
   var orderby = req.body.orderby;
@@ -197,3 +287,5 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+
